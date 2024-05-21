e18210bc634ff2a582572db52c0bbbb393a13ed1
rameeeee <moderato611@naver.com>
2024-05-22 00:27:37 +0900

    [240522] 푸터 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer_info">© 2024 ParkARam PORTFOLIO<br />리액트를 이용하여 제작하였습니다.</div>
+            <div className="footer_info">© 2024 ParkARam PORTFOLIO</div>
         </div>
     )
 }
